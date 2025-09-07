1d0ef0a368f066cc6c1f3c0b493a8befa26beb7a
TsvetkovRA <glhfaaa@gmail.com>
2025-09-07 22:38:06 +0300

    Task1

---
+++ Tyuiu.TsvetkovRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Роман";
+        int age = 23;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " года!");
+        Console.ReadKey();
+    }
+}

d20fddba01cd8c7bf2dbc7e5deb24c212d8f1825
Admin <Admin@DESKTOP-3DJB8S2>
2025-09-18 21:33:12 +0300

    Task3

---
+++ Tyuiu.TsvetkovRA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.TsvetkovRA.Sprint0.Task3.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}

1089fa3a130abe0a2ba4182b0033bd1a4f509157
Admin <Admin@DESKTOP-3DJB8S2>
2025-09-18 22:09:54 +0300

    Task4

---
+++ Tyuiu.TsvetkovRA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.TsvetkovRA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.TsvetkovRA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtraction()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TsvetkovRA.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1, 5));
+
+        Console.WriteLine(DataService.Subtraction(15, 5));
+
+        Console.WriteLine(DataService.Multiplication(10, 10));
+
+        Console.WriteLine(DataService.Division(5, 5));
+
+        Console.ReadKey();
+
+
+    }
+} 
